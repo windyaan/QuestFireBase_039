45211d2a6f970026d8676345cac45a83e40d1d2d
windyaan <windyanurmaretha@gmail.com>
2025-01-13 06:46:34 +0700

    Class FormState untuk menampilkan status

--- app/src/main/java/com/example/myapplication/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/home/viewmodel/InsertViewModel.kt
@@ -62,3 +62,10 @@ class InsertViewModel(
         uiState = FormState.Idle
     }
 }
+
+sealed class FormState {
+    object Idle : FormState()
+    object Loading : FormState()
+    data class Success(val message: String) : FormState()
+    data class Error(val message: String) : FormState()
+}
