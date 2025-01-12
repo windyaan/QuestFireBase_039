c4eff467c62b7f8636364fc850178ab040c7f963
windyaan <windyanurmaretha@gmail.com>
2025-01-13 06:53:11 +0700

    FormErrorState jika terjadi kesalahan dalam pengisian

--- app/src/main/java/com/example/myapplication/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/home/viewmodel/InsertViewModel.kt
@@ -74,3 +74,21 @@ data class InsertUiState(
     val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
     val isEntryValid: FormErrorState = FormErrorState()
 )
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null,
+) {
+    fun isValid() : Boolean {
+        return nim == null
+                && nama == null
+                && jenisKelamin == null
+                && alamat == null
+                && kelas == null
+                && angkatan == null
+    }
+}
