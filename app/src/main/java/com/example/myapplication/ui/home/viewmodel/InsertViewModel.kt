873402e10978807d43b9d09361e896d30edb187a
windyaan <windyanurmaretha@gmail.com>
2025-01-13 06:25:24 +0700

    Mendefinisikan variable dalam class

---
+++ app/src/main/java/com/example/myapplication/ui/home/viewmodel/InsertViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication.ui.home.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.myapplication.repository.RepositoryMhs
+
+class InsertViewModel(
+    private val repositoryMhs: RepositoryMhs
+): ViewModel() {
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+}
