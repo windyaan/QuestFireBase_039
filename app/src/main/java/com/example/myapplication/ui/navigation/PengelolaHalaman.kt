f0ac8f8085c7b1ec8f64ef9dbb27a274fb7d9a00
windyaan <windyanurmaretha@gmail.com>
2025-01-13 10:17:07 +0700

    import package NavHost

--- app/src/main/java/com/example/myapplication/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/myapplication/ui/navigation/PengelolaHalaman.kt
@@ -3,7 +3,11 @@ package com.example.myapplication.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.ui.home.pages.HomeScreen
+import com.example.myapplication.ui.home.pages.InsertMhsView
 
 
 @Composable
