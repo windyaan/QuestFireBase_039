bcab00f1ebd6ee521af02b0321d1047bf1ed9cbf
windyaan <windyanurmaretha@gmail.com>
2025-01-13 08:25:56 +0700

    Menambahkan PengelolaHalaman

---
+++ app/src/main/java/com/example/myapplication/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+
+@Composable
+fun PengelolaHalaman (
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToltemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                }
+            )
+        }
+
+        composable(DestinasiInsert.route){
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = { navController.navigate(DestinasiHome.route) }
+            )
+        }
+    }
+}
