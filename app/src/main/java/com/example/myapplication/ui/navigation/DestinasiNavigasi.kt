7409df303df0194eebcda979ac628b72d54edeb7
windyaan <windyanurmaretha@gmail.com>
2025-01-13 07:54:36 +0700

    Menambahakan DestinasiNavigasi untuk mengelola navigasi halaman

---
+++ app/src/main/java/com/example/myapplication/ui/navigation/DestinasiNavigasi.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication.ui.navigation
+
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route: String = "home"
+    override val titleRes: String = "Home"
+}
+
+object DestinasiInsert : DestinasiNavigasi {
+    override val route: String = "insert"
+    override val titleRes: String = "Insert"
+}
